5fb06df10d4be0590c0093e0b16c0bd42566d169
GitHub Action <action@github.com>
2024-10-21 17:46:22 +0000

    Regenerate artifacts from biolink-model.yaml

--- src/biolink_model/scripts/classprefixes.py
+++ src/biolink_model/scripts/classprefixes.py
@@ -1,5 +1,5 @@
 # Auto generated from class_prefixes.yaml by pythongen.py version: 0.0.1
-# Generation date: 2024-10-14T23:07:37
+# Generation date: 2024-10-21T17:46:13
 # Schema: BiolinkClassPrefixes
 #
 # id: biolink-model-class-prefixes
